e12334f2e5da4e8caa8956ace41a77af31b498ec
Jawad Rashid <jawadrashid2010@gmail.com>
2012-07-03 20:20:04 +0500

    Completed Chapter 6 Exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -96,21 +96,32 @@ describe User do
 	let(:found_user) { User.find_by_email(@user.email) }
 
 	describe "with valid password" do
-	it { should == found_user.authenticate(@user.password) }
+		it { should == found_user.authenticate(@user.password) }
 	end
 
 	describe "with invalid password" do
-	let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-	it { should_not == user_for_invalid_password }
-	specify { user_for_invalid_password.should be_false }
+		it { should_not == user_for_invalid_password }
+		specify { user_for_invalid_password.should be_false }
 	end
   end
 
+	 	 
   describe "with a password that's too short" do
   	before { @user.password = @user.password_confirmation = "a" * 5 }
-  it { should be_invalid }
-end
+  	it { should be_invalid }
+  end
+
+  describe "email address with mixed case" do 
+  	let(:mixed_case_email) {"Foo@ExAMPle.CoM"}
+
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		@user.reload.email.should == mixed_case_email.downcase
+  	end
+  end
 
 end
 
